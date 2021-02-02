4c9162ef94f12ea4ceffd850e3f0b86c9c03b31e
Asif2535 <aiqbal517@gmail.com>
2021-02-02 16:38:32 +0600

    null vs undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,22 @@
+//undefined
+//=========================
+let pakhi;
+console.log(pakhi);
+
+function add(num1,num2){
+    console.log(num1+num2);
+}
+const result =add(13,82);
+console.log(result);
+
+const premik = {name:"smart dude",phone:458421};
+console.log(premik.gf);
+
+let fun = undefined; // ;)
+console.log(fun);
+
+//null
+//=====================
+
+let enjoy = null; //null value set kore dite hoi
+console.log(enjoy);
